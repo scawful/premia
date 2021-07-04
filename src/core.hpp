88eb207d25d08622f7f06e991ae8f8d4aa3300cd
Justin Scofield <justin.scofield@outlook.com>
2021-07-04 00:21:59 -0400

    Rearrange core header, add additional libraries needed for some ideas

--- src/core.hpp
+++ src/core.hpp
@@ -2,6 +2,20 @@
 #ifndef core_hpp
 #define core_hpp
 
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <random>
+#include <map>
+#include <unordered_map>
+#include <fstream>
+#include <vector>
+#include <exception>
+#include <ctime>
+#include <sstream>
+
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
@@ -9,8 +23,11 @@
 #include <boost/tokenizer.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/foreach.hpp>
+#include <boost/date_time/gregorian/gregorian.hpp>
+#include <boost/date_time/posix_time/posix_time_io.hpp>
 
 //#include <ql/quantlib.hpp>
 #include <ql/option.hpp>
@@ -22,16 +39,6 @@
 #include "Libraries/imgui/imgui.h"
 #include "Libraries/imgui/imgui_sdl.h"
 #include "Libraries/imgui/imgui_impl_sdl.h"
-
-#include <stdio.h>
-#include <cstdlib>
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <random>
-#include <map>
-#include <fstream>
-#include <vector>
 
 #define SCREEN_WIDTH 782
 #define SCREEN_HEIGHT 543
