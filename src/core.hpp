5b2c5678484cf528ef7cdb42e7c12ba2cdfeb39c
Justin Scofield <justin.scofield@outlook.com>
2021-07-04 19:02:23 -0400

    Added ImPlot library for advanced charting and plotting of data

--- src/core.hpp
+++ src/core.hpp
@@ -39,6 +39,8 @@
 #include "Libraries/imgui/imgui.h"
 #include "Libraries/imgui/imgui_sdl.h"
 #include "Libraries/imgui/imgui_impl_sdl.h"
+#include "Libraries/imgui/implot.h"
+#include "Libraries/imgui/implot_internal.h"
 
 #define SCREEN_WIDTH 782
 #define SCREEN_HEIGHT 543
