a176e4519be675ddb3f48b18cdcc17e315ad2f66
Justin Scofield <justin.scofield@outlook.com>
2021-08-07 12:43:14 -0400

    Removed http client related code from the Coinbase Pro class

--- src/MarketData/CoinbasePro.hpp
+++ src/MarketData/CoinbasePro.hpp
@@ -12,18 +12,6 @@ namespace cbp
     {
     private:
         boost::shared_ptr<Client> http_client;
-        std::time_t _timestamp;
-        std::string _message;
-        std::string _body;
-        std::string _method;
-        std::string _request_path;
-        std::string _endpoint_url;
-        std::string _access_signature;
-
-        unsigned int _hash_length;
-        std::array<unsigned char, EVP_MAX_MD_SIZE> _hash;
-
-        void get_account_data( std::string filename );
 
     public:
         CoinbasePro();
