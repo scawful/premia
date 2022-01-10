a066a5eb75ff647fadd7f545ed0d39e338c435ca
Justin Scofield <justin.scofield@outlook.com>
2022-01-09 20:47:17 -0500

    Renamed MarketData folder to Services and added KuCoin template

---
+++ src/Services/KuCoin.cpp
@@ -0,0 +1,9 @@
+#include "KuCoin.hpp"
+
+namespace kc 
+{
+    KuCoin::KuCoin()
+    {
+        
+    }
+}
---
+++ src/Services/KuCoin.hpp
@@ -0,0 +1,17 @@
+#ifndef KuCoin_hpp
+#define KuCoin_hpp
+
+#include "../../core.hpp"
+
+namespace kc
+{
+    class KuCoin 
+    {
+    private:
+
+    public:
+        KuCoin();
+    };
+}
+
+#endif 
