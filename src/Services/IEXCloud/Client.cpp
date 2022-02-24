67cc51cdbed3f633bf86787f7e6fc1d84046ffa3
Justin Scofield <justin.scofield@outlook.com>
2022-02-24 16:51:06 -0600

    Developed the IEXCloud API wrapper based on the TDAmeritrade model. Considering make a parent class for them since they have some similarities but not sure if it's worth the plumbing involved.

---
+++ src/Services/IEXCloud/Client.cpp
@@ -0,0 +1,94 @@
+#include "Client.hpp"
+
+std::string iex::Client::current_endpoint()
+{
+    if (sandbox_mode)
+        return this->sandbox_endpoint;
+    else
+        return this->base_endpoint;
+}
+
+size_t iex::Client::json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s)
+{
+    size_t new_length = size * nmemb;
+    try {
+        s->append((char*)contents, new_length);
+    } catch(std::bad_alloc &e) {
+        // handle memory problem
+        return 0;
+    }
+    return new_length;
+}
+
+iex::Client::Client() 
+{
+    this->base_endpoint = "https://cloud.iexapis.com/";
+    this->sandbox_endpoint = "https://sandbox.iexapis.com/stable/";
+    this->token_parameter = "?token=" + IEX_KEY;
+    this->sandbox_mode = true;
+}
+
+/**
+ * @brief 
+ * 
+ * @param endpoint 
+ * @return std::string 
+ */
+std::string iex::Client::send_request(std::string endpoint) 
+{
+    CURL *curl;
+    CURLcode res;
+    std::string response;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, json_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        res = curl_easy_perform(curl);
+
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+    }
+
+    return response;
+}
+
+/**
+ * @brief 
+ * 
+ * @param endpoint 
+ * @return std::string 
+ */
+std::string iex::Client::send_authorized_request(std::string endpoint) 
+{
+    CURL *curl;
+    CURLcode res;
+    std::string response;
+
+    curl = curl_easy_init();
+    if (curl)
+    {
+        struct curl_slist *headers = NULL;
+        std::string auth_bearer = "Authorization: Bearer " + IEX_KEY;
+        headers = curl_slist_append(headers, auth_bearer.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, json_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        res = curl_easy_perform(curl);
+
+        curl_easy_cleanup(curl);
+    }
+
+    return response;
+}
+
+std::string iex::Client::get_fund_ownership(std::string symbol)
+{
+    std::string endpoint = current_endpoint() + "stock/" + symbol + "/fund-ownership/" + token_parameter;
+    return send_request(endpoint);
+}
---
+++ src/Services/IEXCloud/Client.hpp
@@ -0,0 +1,31 @@
+#ifndef IEXClient_hpp
+#define IEXClient_hpp
+
+#include "../../core.hpp"
+#include "../../apikey.hpp"
+#include "Data/FundOwnership.hpp"
+
+namespace iex 
+{
+    class Client 
+    {
+    private:
+        bool sandbox_mode;
+        std::string base_endpoint;
+        std::string sandbox_endpoint;
+        std::string token_parameter;
+
+        std::string current_endpoint();
+        static size_t json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s);
+
+    public:
+        Client();
+
+        std::string send_request(std::string endpoint);
+        std::string send_authorized_request(std::string endpoint);
+
+        std::string get_fund_ownership(std::string symbol);
+    };
+}
+
+#endif
