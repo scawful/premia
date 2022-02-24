a56029916edb2e428b1a667a75ce0b3f5cf541ba
Justin Scofield <justin.scofield@outlook.com>
2022-02-24 16:52:55 -0600

    Added a simple function for setting map items. will add proper member variables later

--- src/Services/TDAmeritrade/Data/UserPrincipals.cpp
+++ src/Services/TDAmeritrade/Data/UserPrincipals.cpp
@@ -4,3 +4,8 @@ tda::UserPrincipals::UserPrincipals()
 {
     
 }
+
+void tda::UserPrincipals::set_account_data(std::unordered_map<std::string, std::string> data)
+{
+    this->account_data = data;
+}
