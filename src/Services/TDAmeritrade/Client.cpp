cbeea3d12ae46f40cd0090e764dcb92de7ae37cc
Justin Scofield <justin.scofield@outlook.com>
2022-02-24 08:25:48 -0600

    Massive Client class upgrade, handles server responses in a string buffer rather than a file now. Much more efficient than it was.

--- src/Services/TDAmeritrade/Client.cpp
+++ src/Services/TDAmeritrade/Client.cpp
@@ -1,42 +1,92 @@
 #include "Client.hpp"
 
-void Client::get_user_principals() 
+/**
+ * @brief Get User Principals from API endpoint
+ *        Parse and store in UserPrincipals object for local use
+ * 
+ */
+void tda::Client::get_user_principals() 
 {
-    
+    std::string endpoint = "https://api.tdameritrade.com/v1/userprincipals?fields=streamerSubscriptionKeys,streamerConnectionInfo";
+    std::string response = send_authorized_request(endpoint);
+    JSONObject::ptree json_principals = parser.read_response(response);
+    user_principals = parser.parse_user_principals(json_principals);
 }
 
-void Client::post_access_token() 
+std::string tda::Client::post_access_token() 
 {
-    
-}
+    CURL *curl;
+    CURLcode res;
+    std::string response;
 
-void Client::post_account_authorization() 
-{
-    
-}
+    res = curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
 
-size_t json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s)
-{
-    size_t newLength = size*nmemb;
-    try
+    if (curl)
     {
-        s->append((char*)contents, newLength);
+        // set the url to receive the POST
+        curl_easy_setopt(curl, CURLOPT_URL, "https://api.tdameritrade.com/v1/oauth2/token");
+
+        // specify we want to post
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, true);
+
+        // set the headers for the request
+        struct curl_slist *headers = NULL;
+        headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+        res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        // chunked request for http1.1/200 ok
+        struct curl_slist *chunk = NULL;
+        chunk = curl_slist_append(chunk, "Transfer-Encoding: chunked");
+        res = curl_easy_setopt(curl, CURLOPT_HTTPHEADER, chunk);
+
+        // specify post data, have to url encode the refresh token
+        std::string easy_escape = curl_easy_escape(curl, refresh_token.c_str(), refresh_token.length());
+        std::string data_post = "grant_type=refresh_token&refresh_token=" + easy_escape + "&client_id=" + TDA_API_KEY;
+
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data_post.c_str());
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, data_post.length());
+
+        // write data from the url to the function
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, json_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        // specify the user agent
+        curl_easy_setopt(curl, CURLOPT_USERAGENT, "premia-agent/1.0");
+
+        // run the operations
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+        // cleanup yeah yeah
+        curl_easy_cleanup(curl);
     }
-    catch(std::bad_alloc &e)
-    {
-        //handle memory problem
+
+    curl_global_cleanup();
+    return response;
+}
+
+size_t tda::Client::json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s)
+{
+    size_t new_length = size * nmemb;
+    try {
+        s->append((char*)contents, new_length);
+    } catch(std::bad_alloc &e) {
+        // handle memory problem
         return 0;
     }
-    return newLength;
+    return new_length;
 }
 
 /**
  * @brief Construct a new Client:: Client object
  * 
  */
-Client::Client() 
+tda::Client::Client() 
 {
-    
+    this->access_token = "";
 }
 
 /**
@@ -45,65 +95,64 @@ Client::Client()
  * @param endpoint 
  * @return JSONObject 
  */
-JSONObject Client::send_request(std::string endpoint) 
+std::string tda::Client::send_request(std::string endpoint) 
 {
     CURL *curl;
-    FILE *fp;
     CURLcode res;
+    std::string response;
 
     curl = curl_easy_init();
     if (curl)
     {
-        fp = fopen(filename.c_str(), "wb");
         curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, json_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
         res = curl_easy_perform(curl);
 
         /* always cleanup */
         curl_easy_cleanup(curl);
-        fclose(fp);
     }
 
-    // ==========
+    return response;
+}
 
-    std::time_t now = std::time(0);
-    std::string output_file_name = this->_current_ticker + "_" + std::to_string(now) + ".json";
-    download_file(new_url, output_file_name);
-    std::ifstream jsonFile(output_file_name, std::ios::in | std::ios::binary);
-    JSONObject::ptree propertyTree;
+std::string tda::Client::send_authorized_request(std::string endpoint) 
+{
+    CURL *curl;
+    CURLcode res;
+    std::string response;
 
-    try
+    curl = curl_easy_init();
+    if (curl)
     {
-        read_json(jsonFile, propertyTree);
+        struct curl_slist *headers = NULL;
+        std::string auth_bearer = "Authorization: Bearer " + access_token;
+        headers = curl_slist_append(headers, auth_bearer.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+        curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, json_write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        res = curl_easy_perform(curl);
+
+        curl_easy_cleanup(curl);
     }
-    catch (std::exception &json_parser_error)
-    {
-        SDL_Log("%s", json_parser_error.what());
-    }
 
-    jsonFile.close();
-    std::remove(output_file_name.c_str());
-
-    return propertyTree;
+    return response;
 }
 
-JSONObject Client::send_authorized_request(std::string endpoint) 
+void tda::Client::start_session() 
 {
     
 }
 
-void Client::start_session() 
+void tda::Client::send_session_request(std::string request) 
 {
-    
+    websocket_session->send_message(std::make_shared<std::string const>(request));
 }
 
-void Client::send_session_request() 
+void tda::Client::send_interrupt_signal() 
 {
-    
-}
-
-void Client::send_interrupt_signal() 
-{
-    
+    websocket_session->interrupt();
 }
--- src/Services/TDAmeritrade/Client.hpp
+++ src/Services/TDAmeritrade/Client.hpp
@@ -2,34 +2,47 @@
 #define Client_hpp
 
 #include "../../core.hpp"
+#include "../../apikey.hpp"
+#include "Parser.hpp"
+#include "Session.hpp"
+#include "Data/UserPrincipals.hpp"
+
+namespace JSONObject = boost::property_tree;
 
 namespace tda 
 {
-    namespace JSONObject = boost::property_tree;
-    
+
     class Client 
     {
     private:
+        // API Strings 
         std::string api_key;
         std::string refresh_token;
         std::string access_token;
 
-        JSONObject::ptree user_principals;
+        // API Data 
+        Parser parser;
+        UserPrincipals user_principals;
 
+        // WebSocket session variables 
+        boost::asio::io_context ioc;
+        std::shared_ptr<tda::Session> websocket_session;
+        std::shared_ptr<std::vector<std::string>> websocket_buffer;
+
+        // API Functions 
         void get_user_principals();
-        void post_access_token();
-        void post_account_authorization();
+        std::string post_access_token();
 
-        size_t json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s);
+        static size_t json_write_callback(void *contents, size_t size, size_t nmemb, std::string *s);
 
     public:
         Client();
 
-        JSONObject send_request(std::string endpoint);
-        JSONObject send_authorized_request(std::string endpoint);
+        std::string send_request(std::string endpoint);
+        std::string send_authorized_request(std::string endpoint);
 
         void start_session();
-        void send_session_request();
+        void send_session_request(std::string request);
         void send_interrupt_signal();
 
     };
