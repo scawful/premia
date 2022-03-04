d7a5bb2a2255f837841a3b339293ad91dc857db4
Justin Scofield <justin.scofield@outlook.com>
2022-03-04 15:08:22 -0600

    removed these (for now probably maybe)

--- src/Services/TDAmeritrade/Data/Quote.hpp
+++ src/Services/TDAmeritrade/Data/Quote.hpp
@@ -11,25 +11,6 @@ namespace tda
         boost::property_tree::ptree quoteData;
         std::map<std::string, std::string> quoteVariables;
 
-        std::string symbol;
-        std::string description;
-        double bidPrice, askPrice, lastPrice, openPrice, highPrice, lowPrice, closePrice;
-        double bidSize, askSize, lastSize, bidId, askId, lastId;
-        double netChange, totalVolume;
-        double quoteTimeInLong, tradeTimeInLong;
-        double mark;
-        std::string exchange;
-        std::string exchangeView;
-        bool marginable;
-        bool shortable;
-        double digits;
-        double _52WkHigh, _52WkLow;
-        double peRatio;
-        double divAmount, divYield;
-        std::string divDate;
-        std::string securityStatus;
-        double regularMarketLastPrice, regularMarketLastSize, regularMarketNetChange, regularMarketTradeTimeInLong;
-
     public:
         Quote();
 
