3d52540de6bda49bdb71a7042fd13644eb149c32
Justin Scofield <justin.scofield@outlook.com>
2022-03-05 16:28:21 -0600

    Started Halext interface class for application backend management

---
+++ src/Services/Halext/Halext.cpp
@@ -0,0 +1,7 @@
+#include "Halext.hpp"
+
+
+Halext::Halext() 
+{
+    
+}
---
+++ src/Services/Halext/Halext.hpp
@@ -0,0 +1,13 @@
+#ifndef Halext_hpp
+#define Halext_hpp
+
+#include "../../core.hpp"
+
+class Halext
+{
+public:
+    Halext();
+
+};
+
+#endif
