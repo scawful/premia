2be70c42e0f78a70913f3e09cd70b77d354906da
Justin Scofield <justin.scofield@outlook.com>
2022-03-05 16:24:16 -0600

    Overhauled the LoginFrame to be the main Login rather than API keys

--- src/State/Frames/LoginFrame.cpp
+++ src/State/Frames/LoginFrame.cpp
@@ -8,53 +8,26 @@ LoginFrame::LoginFrame() : Frame()
     this->port = 7497;
 }
 
+bool LoginFrame::checkLoginStatus()
+{
+    return isLoggedIn;
+}
+
 void LoginFrame::update() 
 {
-    ImGui::Text("Select your broker/exchange API to use account features like trading and portfolio analysis.");
+    static char username[128] = "";
+    static char password[128] = "";
 
-    static char host_char[128] = "127.0.0.1";
-    if (!isLoggedIn)
-    {
-        ImGuiTabBarFlags tab_bar_flags = ImGuiTabBarFlags_None;
-        if (ImGui::BeginTabBar("MyTabBar", tab_bar_flags))
-        {
-            if (ImGui::BeginTabItem("TDAmeritrade"))
-            {
-                ImGui::Spacing();
-                static char api_key[128] = "";
-                static char refresh_token[128] = "";
-                ImGui::InputText("Consumer Key", api_key, IM_ARRAYSIZE(api_key));
-                ImGui::InputText("Refresh Token", refresh_token, IM_ARRAYSIZE(refresh_token));
-                ImGui::Button("Login");
-                ImGui::SameLine();
-                if (ImGui::Button("Fetch from file")) {
-                    *tda_logged_in = true;
-                    *public_mode = false;
-                }
-                ImGui::EndTabItem();
-            }
-            if (ImGui::BeginTabItem("IEXCloud"))
-            {
-                ImGui::Spacing();
-                static char api_key[128] = "";
-                ImGui::InputText("API Key", api_key, IM_ARRAYSIZE(api_key));
-                ImGui::Button("Login");
-                ImGui::EndTabItem();
-            }
-            if (ImGui::BeginTabItem("CoinbasePro"))
-            {
-                ImGui::Spacing();
-                static char api_key[128] = "";
-                static char secret_key[128] = "";
-                static char passphrase[128] = "";
-                ImGui::InputText("API Key", api_key, IM_ARRAYSIZE(api_key));
-                ImGui::InputText("Secret Key", secret_key, IM_ARRAYSIZE(secret_key));
-                ImGui::InputText("Passphrase", passphrase, IM_ARRAYSIZE(passphrase));
-                ImGui::Button("Login");
-                ImGui::EndTabItem();
-            }
-            ImGui::EndTabBar();
-        }
+    std::string welcomePrompt = "Welcome to Premia"; 
+    auto windowWidth = ImGui::GetWindowSize().x;
+    auto textWidth   = ImGui::CalcTextSize(welcomePrompt.c_str()).x;
+    ImGui::SetCursorPosX((windowWidth - textWidth) * 0.5f);
+    ImGui::Text("%s", welcomePrompt.c_str());
+
+    ImGui::Text("Username: "); ImGui::SameLine(); ImGui::InputText("##username",username, IM_ARRAYSIZE(username));
+    ImGui::Text("Password: "); ImGui::SameLine(); ImGui::InputText("##password",password, IM_ARRAYSIZE(password));
+    
+    if ( ImGui::Button("Login") ) {
+        isLoggedIn = true;
     }
-
 }
--- src/State/Frames/LoginFrame.hpp
+++ src/State/Frames/LoginFrame.hpp
@@ -16,6 +16,7 @@ private:
 public:
     LoginFrame();
 
+    bool checkLoginStatus();
     void update();
 };
 
