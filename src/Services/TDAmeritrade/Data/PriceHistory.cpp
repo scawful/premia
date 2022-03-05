cd94e6dd6abb47de2762263c6a1070a1bdf5b05f
Justin Scofield <justin.scofield@outlook.com>
2022-03-05 16:20:39 -0600

    Overhaul and remove old PriceHistory self parsing

--- src/Services/TDAmeritrade/Data/PriceHistory.cpp
+++ src/Services/TDAmeritrade/Data/PriceHistory.cpp
@@ -2,80 +2,34 @@
 
 using namespace tda;
 
-/* =============== PriceHistory Class =============== */
-
-void PriceHistory::initVariables()
-{
-    for ( auto& history_it: priceHistoryData )
-    {
-        if ( history_it.first == "candles" )
-        {
-            for ( auto& candle_it: history_it.second )
-            {
-                tda::Candle newCandle;
-                std::string datetime;
-                std::pair<double, double> high_low;
-                std::pair<double, double> open_close;
-
-                for ( auto& candle2_it: candle_it.second )
-                {
-                    if ( candle2_it.first == "high" )
-                        high_low.first = boost::lexical_cast<double>(candle2_it.second.get_value<std::string> () );
-                    if ( candle2_it.first == "low" )
-                        high_low.second = boost::lexical_cast<double>(candle2_it.second.get_value<std::string> () );
-                    
-                    if ( candle2_it.first == "open" )
-                        open_close.first = boost::lexical_cast<double>(candle2_it.second.get_value<std::string> () );
-                    if ( candle2_it.first == "close" )
-                        open_close.second = boost::lexical_cast<double>(candle2_it.second.get_value<std::string> () );
-
-                    if ( candle2_it.first == "volume" )
-                        newCandle.volume = stoi( candle2_it.second.get_value<std::string> () );
-                        
-                    if ( candle2_it.first == "datetime" )
-                    {
-                        std::stringstream dt_ss;
-                        std::time_t secsSinceEpoch = boost::lexical_cast<std::time_t>(candle2_it.second.get_value<std::string> ());
-                        newCandle.raw_datetime = secsSinceEpoch;
-                        secsSinceEpoch *= 0.001;
-
-                        //%a %d %b %Y - %I:%M:%S%p
-                        //%H:%M:%S
-                        dt_ss << std::put_time(std::localtime(&secsSinceEpoch), "%a %d %b %Y - %I:%M:%S%p");
-                        datetime = dt_ss.str();
-                        //datetime = boost::lexical_cast<std::string>(candle2_it.second.get_value<std::string> ());
-                    }
-                    
-                }
-
-                newCandle.datetime = datetime;
-                newCandle.highLow = high_low;
-                newCandle.openClose = open_close;
-
-                candleVector.push_back( newCandle );
-            }
-        }
-        else if ( history_it.first == "symbol" )
-        {
-            priceHistoryVariables["symbol"] = history_it.second.get_value<std::string>();
-        }
-    }
-}
-
 PriceHistory::PriceHistory() 
 {
-    
-}
-
-PriceHistory::PriceHistory( boost::property_tree::ptree price_history_data )
-{
-    priceHistoryData = price_history_data;
-    initVariables();
+    this->tickerSymbol = "";
+    this->initialized = false;
+    for (int i = 0; i < 4; i++ ) {
+        std::vector<tda::Candle> vector;
+        candles.push_back(vector);
+    }
 }
 
 void PriceHistory::addCandle(tda::Candle candle)
 {
     candleVector.push_back(candle);
+}
+
+void PriceHistory::addCandleByType(Candle candle, int type)
+{
+    candles[type].push_back(candle);
+}
+
+std::vector<tda::Candle> PriceHistory::getCandles(int index)
+{
+    return candles[index];
+}
+
+int PriceHistory::getNumCandles(int index)
+{
+    return candles[index].size();
 }
 
 std::vector< tda::Candle > PriceHistory::getCandleVector()
@@ -93,9 +47,29 @@ std::string PriceHistory::getPriceHistoryVariable( std::string variable )
     return priceHistoryVariables[variable];
 }
 
+std::string PriceHistory::getTickerSymbol()
+{
+    return tickerSymbol;
+}
+
+bool PriceHistory::getInitialized()
+{
+    return initialized;
+}
+
 void PriceHistory::setPriceHistoryVariable(std::string key, std::string value)
 {
     priceHistoryVariables[key] = value;
+}
+
+void PriceHistory::setTickerSymbol(std::string ticker)
+{
+    this->tickerSymbol = ticker;
+}
+
+void PriceHistory::setInitialized()
+{
+    this->initialized = true;
 }
 
 void PriceHistory::clear()
--- src/Services/TDAmeritrade/Data/PriceHistory.hpp
+++ src/Services/TDAmeritrade/Data/PriceHistory.hpp
@@ -23,27 +23,34 @@ namespace tda
     class PriceHistory
     {
     private:
-        std::vector<tda::Candle> minute_candles;
-        std::vector<tda::Candle> daily_candles;
-        std::vector<tda::Candle> weekly_candles;
-        std::vector<tda::Candle> monthly_candles;
+        bool initialized;
+        std::string tickerSymbol;
+        std::vector<std::vector<tda::Candle>> candles;
 
-        boost::property_tree::ptree priceHistoryData;
         std::map<std::string, std::string> priceHistoryVariables;
         std::map<std::string, std::string> candleData;
         std::vector< tda::Candle > candleVector;
-
-        void initVariables();
             
     public:
         PriceHistory();
-        PriceHistory(boost::property_tree::ptree price_history_data);
 
         void addCandle(tda::Candle candle);
+        void addCandleByType(Candle candle, int type);
+
+        std::vector<tda::Candle> getCandles(int index);
+        int getNumCandles(int index);
+
         std::vector< tda::Candle > getCandleVector();
         std::string getCandleDataVariable( std::string variable );
         std::string getPriceHistoryVariable( std::string variable );
+        std::string getTickerSymbol();
+        bool getInitialized();
+
         void setPriceHistoryVariable(std::string key, std::string value);
+        void setTickerSymbol(std::string ticker);
+        void setInitialized();
+
+        void updatePriceHistory();
 
         void clear();
     };
