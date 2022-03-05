cdbffeba594c660d1af6feb30af33c39ba9b48c8
Justin Scofield <justin.scofield@outlook.com>
2022-03-05 17:07:39 -0600

    Added Client class for Halext service

---
+++ src/Services/Halext/Client.cpp
@@ -0,0 +1,12 @@
+#include "Client.hpp"
+
+void Client::establish_connection() 
+{
+    // const char *url = (argc > 1 ? argv[1] : "mysqlx://root@127.0.0.1");
+ 
+    // Session sess(url);
+}
+
+Client::Client() {
+    
+}
---
+++ src/Services/Halext/Client.hpp
@@ -0,0 +1,15 @@
+#ifndef HalextClient_hpp
+#define HalextClient_hpp
+
+#include "../../core.hpp"
+
+class Client 
+{
+private:
+    void establish_connection();
+    
+public:
+    Client();
+};
+
+#endif
