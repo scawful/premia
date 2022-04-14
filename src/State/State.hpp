87cfc90334051b916b82e2f5eabbd8783449f2b2
Justin Scofield <justin.scofield@outlook.com>
2022-04-13 21:00:07 -0500

    Refactor + const correctness

--- src/State/State.hpp
+++ src/State/State.hpp
@@ -7,7 +7,7 @@
 class State
 {
 public:
-    virtual void init( Manager *premia ) = 0;
+    virtual void init(Manager *premia) = 0;
     virtual void cleanup() = 0;
     
     virtual void pause() = 0;
@@ -17,13 +17,15 @@ public:
     virtual void update() = 0;
     virtual void draw() = 0;
     
-    void changeState( Manager *premia, State *state )
+    void changeState(Manager *premia, State *state) const
     {
         premia->change(state);
     }
     
 protected:
-    State() { }
+    State()=default;
+    ~State()=default;
+
 };
 
 #endif /* State_hpp */
