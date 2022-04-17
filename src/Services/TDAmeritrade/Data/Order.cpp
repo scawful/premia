7bde4207294875a03491e1698cccfb0fffe04879
scawful <justin.scofield@outlook.com>
2022-04-17 16:45:52 -0500

    Started formally making the Order class

--- src/Services/TDAmeritrade/Data/Order.cpp
+++ src/Services/TDAmeritrade/Data/Order.cpp
@@ -1,7 +1,46 @@
 #include "Order.hpp"
 using namespace tda;
 
-Order::Order() 
+void Order::prepareOrderString() const
 {
-    
+    boost::property_tree::ptree orderPropertyTree;
+
+    orderPropertyTree.add_child("orderType", getOrderType());
+    orderPropertyTree.add_child("session", getSession());
+    orderPropertyTree.add_child("duration", getDuration());
+    orderPropertyTree.add_child("price", getPrice());
+    orderPropertyTree.add_child("orderStrategyType", getOrderStrategyType());
+
+    try {
+        write_json(orderJSON, orderPropertyTree);
+    } catch (const boost::property_tree::ptree_error & e) {
+        e.what();
+    }
+}
+
+Order::Order()=default;
+Order::~Order()=default;
+
+std::string Order::getString() const {
+    return orderJSON;
+}
+
+std::string Order::getOrderType() const {
+    return orderType;
+}
+
+std::string Order::getSession() const {
+    return session;
+}
+
+std::string Order::getDuration() const {
+    return duration;
+}
+
+std::string Order::getOrderStrategyType() const {
+    return orderStrategyType;
+}
+
+std::string Order::getPrice() const {
+    return price;
 }
--- src/Services/TDAmeritrade/Data/Order.hpp
+++ src/Services/TDAmeritrade/Data/Order.hpp
@@ -8,11 +8,70 @@ namespace tda
     class Order
     {
     private:
+        struct OrderLegCollection {
+            int quantity;
+            std::string symbol;
+            std::string assetType;
+            std::string instruction;
+        };
+
+        std::string orderType;
+        std::string price;
+        std::string session;
+        std::string duration;
+        std::string orderStrategyType;
+        std::string complexOrderStrategyType;
+        std::vector<OrderLegCollection> orderLegCollection;
+
+        std::string orderJSON;
+
+        void prepareOrderString() const;
 
     public:
         Order();
+        ~Order();
 
+        std::string getString() const;
+        std::string getOrderType() const;
+        std::string getSession() const;
+        std::string getDuration() const;
+        std::string getOrderStrategyType() const;
+        std::string getPrice() const;
     };
+
+    // Buy Market: Stock
+    // "orderType": "MARKET",
+    // "session": "NORMAL",
+    // "duration": "DAY",
+    // "orderStrategyType": "SINGLE",
+    // "orderLegCollection": [
+    //     {
+    //     "instruction": "Buy",
+    //     "quantity": 15,
+    //     "instrument": {
+    //         "symbol": "XYZ",
+    //         "assetType": "EQUITY"
+    //     }
+    //     }
+    // ]
+
+    // Buy Limit: Single Option
+    // "complexOrderStrategyType": "NONE",
+    // "orderType": "LIMIT",
+    // "session": "NORMAL",
+    // "price": "6.45",
+    // "duration": "DAY",
+    // "orderStrategyType": "SINGLE",
+    // "orderLegCollection": [
+    //     {
+    //     "instruction": "BUY_TO_OPEN",
+    //     "quantity": 10,
+    //     "instrument": {
+    //         "symbol": "XYZ_032015C49",
+    //         "assetType": "OPTION"
+    //         }
+    //     }
+    // ]
 }
 
 #endif
