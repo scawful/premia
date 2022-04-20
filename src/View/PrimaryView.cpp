47ffc878e3681d0ff5612eec5d2de29f74a54da7
scawful <justin.scofield@outlook.com>
2022-04-20 01:38:21 -0500

    Updated the PrimaryView test layout

--- src/View/PrimaryView.cpp
+++ src/View/PrimaryView.cpp
@@ -7,5 +7,19 @@ void PrimaryView::addEvent(const VoidEventHandler & event)
 
 void PrimaryView::update() 
 {
+    ImGui::NewFrame();
+    ImGui::SetNextWindowPos( ImVec2(0, 0) );
+    const ImGuiIO & io = ImGui::GetIO();
+
+    ImVec2 dimensions(io.DisplaySize.x, io.DisplaySize.y);
+    ImGui::SetNextWindowSize(dimensions, ImGuiCond_Always);
+
+    ImGuiWindowFlags flags = ImGuiWindowFlags_MenuBar;
+    if (!ImGui::Begin("Premia", nullptr, flags)) {
+        ImGui::End();
+        return;
+    }
     ImGui::Text("Primary State");
+
+    ImGui::End();
 }
