7cf2fe1b0d0796de4ca45bfaaacc4a881bb129fa
scawful <justin.scofield@outlook.com>
2022-04-20 01:40:00 -0500

    Experimentation with the MVC startup via the DebugController

--- src/Premia.cpp
+++ src/Premia.cpp
@@ -1,17 +1,35 @@
 #define SDL_MAIN_HANDLED
+#include "Controller/PrimaryController.hpp"
+#include "Controller/DebugController.hpp"
 #include "State/PrimaryState.hpp"
 #include "State/LoginState.hpp"
 
 int main(int argc, char *argv[]) 
 {
-    Manager premia;
-    premia.init(300,200);
-    premia.change(LoginState::instance());
-    while (premia.running()) {
-        premia.handleEvents();
-        premia.update();
-        premia.draw();
+    VoidEventHandler legacyPremia = []() -> void { 
+        Manager premia;
+        premia.init(300,200);
+        premia.change(LoginState::instance());
+        while (premia.running()) {
+            premia.handleEvents();
+            premia.update();
+            premia.draw();
+        }
+        premia.cleanup();
+    };
+
+    DebugView debug;
+    ViewManager vm(debug, legacyPremia);
+    PrimaryModel model;
+    DebugController controller(model, vm);
+
+    controller.onEntry();
+    while (controller.isActive()) 
+    {
+        controller.onInput();
+        controller.onLoad();
+        controller.doRender();
     }
-    premia.cleanup();
+    controller.onExit();
     return 0;
 }
