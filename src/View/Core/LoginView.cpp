f8083ef69f543df4104fc22f788c93010850382d
scawful <justin.scofield@outlook.com>
2022-04-23 16:29:28 -0500

    Relocated LoginView

--- src/View/Core/LoginView.cpp
+++ src/View/Core/LoginView.cpp
@@ -12,17 +12,14 @@ void LoginView::drawScreen() const
         return;
     }
 
-    static char username[128] = "";
-    static char password[128] = "";
-
     std::string welcomePrompt = "Welcome to Premia"; 
     auto windowWidth = ImGui::GetWindowSize().x;
     auto textWidth   = ImGui::CalcTextSize(welcomePrompt.c_str()).x;
     ImGui::SetCursorPosX((windowWidth - textWidth) * 0.5f);
     ImGui::Text("%s", welcomePrompt.c_str());
 
-    ImGui::Text("Username: "); ImGui::SameLine(); ImGui::InputText("##username",username, IM_ARRAYSIZE(username));
-    ImGui::Text("Password: "); ImGui::SameLine(); ImGui::InputText("##password",password, IM_ARRAYSIZE(password));
+    ImGui::Text("Username: "); ImGui::SameLine(); ImGui::InputText("##username", username, IM_ARRAYSIZE(username));
+    ImGui::Text("Password: "); ImGui::SameLine(); ImGui::InputText("##password", password, IM_ARRAYSIZE(password));
     
     if ( ImGui::Button("Login") ) {
         events.at("login")();
--- src/View/Core/LoginView.hpp
+++ src/View/Core/LoginView.hpp
@@ -1,13 +1,16 @@
 #ifndef LoginView_hpp
 #define LoginView_hpp
 
-#include "View.hpp"
-#include "../Model/Model.hpp"
+#include "../View.hpp"
+#include "../../Model/Model.hpp"
 
 class LoginView : public View
 {
 private:
     bool initialized = false;
+    static char username[128];
+    static char password[128];
+    
     VoidEventHandler loginEvent;
     std::unordered_map<std::string, VoidEventHandler> events;
 
