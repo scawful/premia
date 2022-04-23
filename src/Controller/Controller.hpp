fa599994e4ee35f2abd7489df91d805594e522f8
scawful <justin.scofield@outlook.com>
2022-04-23 16:32:23 -0500

    Adjusted headers

--- src/Controller/Controller.hpp
+++ src/Controller/Controller.hpp
@@ -2,10 +2,11 @@
 #define Controller_hpp
 
 #include "../core.hpp"
-#include "../Model/PrimaryModel.hpp"
+#include "../Model/Model.hpp"
 #include "../View/ViewManager.hpp"
-#include "../View/LoginView.hpp"
+#include "../View/Core/LoginView.hpp"
 #include "../View/Dev/DebugView.hpp"
+#include "../View/Chart/ChartView.hpp"
 
 class Controller
 {
