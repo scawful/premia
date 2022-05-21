82b9ed4b1da6bef6296d7ac281ee1a5b4957b3cd
Justin Scofield <justin.scofield@outlook.com>
2022-05-21 14:25:49 -0400

    Update TWS interface

--- include/Interface/TWS.hpp
+++ include/Interface/TWS.hpp
@@ -1,4 +1,4 @@
-#define TraderWorkstationApiInterface_hpp
+#ifndef TraderWorkstationApiInterface_hpp
 #define TraderWorkstationApiInterface_hpp
 
 #include "Library/IBKR.hpp"
@@ -11,7 +11,7 @@ namespace tws {
         TWS() = default;
 
     public:
-        TWS(TDA const&)             = delete;
+        TWS(TWS const&)             = delete;
         void operator=(TWS const&)  = delete;
         static TWS& getInstance() {
             static TWS instance;    
@@ -22,6 +22,8 @@ namespace tws {
         auto runClient(String host, int port, String options) 
             -> void {          
             int clientId = 0;
+            constexpr auto MAX_ATTEMPTS = 10;
+            constexpr auto SLEEP_TIME = 5;
             unsigned attempt = 0;
 
             if (port <= 0)
@@ -29,23 +31,24 @@ namespace tws {
 
             for (;;) {
                 ++attempt;
-                // printf( "Attempt %u of %u\n", attempt, MAX_ATTEMPTS); 50 
+                printf( "Attempt %u of %u\n", attempt, MAX_ATTEMPTS);
                 Client client;
 
                 if (!options.empty()) {
                     client.setConnectOptions(options);
                 }
 
-                client.connect(host, post, clientId);
+                client.connect(host.c_str(), port, clientId);
                 while(client.isConnected()) {
                     client.processMessages();
                 }
-                if (attempt >= 5) {
+
+                if (attempt >= MAX_ATTEMPTS) {
                     break;
                 }
 
-                // printf( "Sleeping %u seconds before next attempt\n", SLEEP_TIME);
-		        // std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
+                printf( "Sleeping %u seconds before next attempt\n", SLEEP_TIME);
+		        std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
             }
         }
 
