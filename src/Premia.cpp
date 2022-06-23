735bf52982e76848b41aba8d4b6520a35edd79bd
Justin Scofield <justin.scofield@outlook.com>
2022-06-23 11:10:46 -0400

    remove lambda macro until i figure out what's going on with windows

--- src/Premia.cpp
+++ src/Premia.cpp
@@ -3,17 +3,12 @@
 
 int main(int argc, char *argv[]) {
   Premia::Controller controller;
-  Try {
-    controller.onEntry();
-    while (controller.isActive()) {
-      controller.onInput();
-      controller.onLoad();
-      controller.doRender();
-    }
-  } catch (const Premia::FatalException &e) {
-    std::cout << e.what() << std::endl;
-  } finally { 
-    controller.onExit(); 
-  } Proceed;
+  controller.onEntry();
+  while (controller.isActive()) {
+    controller.onInput();
+    controller.onLoad();
+    controller.doRender();
+  }
+  controller.onExit();
   return EXIT_SUCCESS;
 }
