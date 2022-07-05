b8dc37451de528b2431d2f2b0a72efd0c999aed5
Justin Scofield <justin.scofield@outlook.com>
2022-07-05 19:32:08 -0400

    protobuf experimentation

---
+++ src/Services/TDAmeritrade/Data/account_handler.cc
@@ -0,0 +1,14 @@
+#include <google/protobuf/message.h>
+
+#include "account_service.pb.h"
+
+class AccountServiceImpl final : public Account::Service {
+  Status GetAccount(ServerContext* context, const AccountRequest* request,
+                    AccountResponse* reply) override {
+    // Call the curl code to get the account and parse it into an object for the
+    // gui to process
+  }
+
+  Status GetAccount(ServerContext* context, const AccountRequest* request,
+                    AccountsResponse* response) override {}
+};
