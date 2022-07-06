9f9baa3045c8fee38ab76af6dd33afdb729d81c6
Justin Scofield <justin.scofield@outlook.com>
2022-07-06 05:30:45 -0400

    rearrange protobuf files

--- src/Services/TDAmeritrade/handler/account_handler.cc
+++ src/Services/TDAmeritrade/handler/account_handler.cc
@@ -6,15 +6,17 @@ namespace premia {
 namespace tda {
 
 class AccountServiceImpl final : public Account::Service {
-  Status GetAccount(ServerContext* context, const AccountRequest* request,
+  Status GetAccount(const AccountRequest* request,
                     AccountResponse* reply) override {
     // Call the curl code to get the account and parse it into an object for the
     // gui to process
   }
 
-  Status GetAccount(ServerContext* context, const AccountRequest* request,
+  Status GetAccount(const AccountRequest* request,
                     AccountsResponse* response) override {}
 };
+
+void test_function() { AccountServiceImpl account_service; }
 
 }  // namespace tda
 }  // namespace premia
