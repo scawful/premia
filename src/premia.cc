a0e5bc6c3e581e908bf93ad0773f4b6df4502d71
Justin Scofield <47263509+scawful@users.noreply.github.com>
2022-08-06 12:39:18 -0400

    error handling

--- src/premia.cc
+++ src/premia.cc
@@ -2,7 +2,9 @@
 
 int main(int argc, char *argv[]) {
   premia::Controller controller;
-  controller.onEntry();
+  if (controller.onEntry().ok()) {
+    return EXIT_FAILURE;
+  }
   while (controller.isActive()) {
     controller.onInput();
     controller.onLoad();
